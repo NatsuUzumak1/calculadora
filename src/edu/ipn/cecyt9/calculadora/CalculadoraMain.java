2b0e08c235b7cc2c2c9da33a3351575e82e0e31d
Alumno <Alumno@LAB-NT05>
2017-08-14 08:24:50 -0500

    Calculadora Java Swing

---
+++ src/edu/ipn/cecyt9/calculadora/CalculadoraMain.java
@@ -0,0 +1,9 @@
+
+package edu.ipn.cecyt9.calculadora;
+
+public class CalculadoraMain {
+ public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		calculadora.setVisible(true);
+	}   
+}
